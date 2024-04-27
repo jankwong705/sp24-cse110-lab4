d9fcf7c936c44b6687d4bc019634b1399b3082e7
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2024-04-27 00:57:34 -0700

    finished part2

--- expose/javascript/part2-question.js
+++ expose/javascript/part2-question.js
@@ -1,13 +1,12 @@
 
 function discountPrices(prices, discount) {
-    var discounted = [];
-    var finalPrice = 0;
-    for (var i = 0; i < prices.length; i++) {
-        var discountedPrice = prices[i] * (1 - discount);
-        finalPrice = Math.round(discountedPrice * 100) / 100;
-        discounted.push(finalPrice);
+    const discounted = [];
+    const length = prices.length;
+    for (let i = 0; i < length; i++) {
+        const discountedPrice = prices[i] * (1 - discount);
+        discounted.push(discountedPrice);
     }
-    //console.log(finalPrice);
+
     return discounted;
 }
 d = discountPrices([100, 200, 300], 0.5);
